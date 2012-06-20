b026579da6a5af43a171add35bbe6207ccbe20c4
Daniel Harężlak <d.harezlak@cyfronet.pl>
2012-06-20 14:07:38 +0000

    webdav testing initialized

---
+++ coinportlet/src/test/java/pl/cyfronet/coin/webdav/WebDavTest.java
@@ -0,0 +1,23 @@
+package pl.cyfronet.coin.webdav;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.HttpException;
+import org.junit.Test;
+
+import com.googlecode.sardine.DavResource;
+import com.googlecode.sardine.Sardine;
+import com.googlecode.sardine.SardineFactory;
+
+public class WebDavTest {
+	@Test
+	public void connect() throws HttpException, IOException {
+		Sardine sardine = SardineFactory.begin("login", "password");
+		List<DavResource> resources = sardine.getResources("http://yourdavserver.com/adirectory/");
+		
+		for (DavResource res : resources) {
+		     System.out.println(res);
+		}
+	}
+}
