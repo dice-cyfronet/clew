9aa6de419638d2e4bbb90d149219c2065be29c1e
Daniel Harezlak <d.harezlak@scantech.com.pl>
2012-11-18 15:21:31 +0100

    LOBCDER test reads spring configuration now

--- coinportlet/src/test/java/pl/cyfronet/coin/webdav/LobcderTest.java
+++ coinportlet/src/test/java/pl/cyfronet/coin/webdav/LobcderTest.java
@@ -14,18 +14,32 @@ import org.apache.http.HttpException;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.googlecode.sardine.DavResource;
 import com.googlecode.sardine.Sardine;
 import com.googlecode.sardine.SardineFactory;
 
-public class WebDavTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/coinportlet-app-ctx.xml")
+public class LobcderTest {
+	private static final Logger log = LoggerFactory.getLogger(LobcderTest.class);
+	
+	@Value("${lobcder.user}") private String webDavUser;
+	@Value("${lobcder.password}") private String webDavPassword;
+	@Value("${lobcder.url}") private String webDavUrl;
+	
 	@Test
 	@Ignore
 	public void connectAndList() throws HttpException, IOException, KeyManagementException, NoSuchAlgorithmException {
-		Sardine sardine = SardineFactory.begin("todo", "todo", createNaiveSslSocketFactory());
-//		List<DavResource> resources = sardine.getResources("https://149.156.10.138:8444/lobcder-1.1-SNAPSHOT");
-		List<DavResource> resources = sardine.getResources("http://149.156.10.138:8080/lobcder-1.0-SNAPSHOT");
+		log.info("Connecting to LOBCDER service at {}", webDavUrl);
+		Sardine sardine = SardineFactory.begin(webDavUser, webDavPassword, createNaiveSslSocketFactory());
+		List<DavResource> resources = sardine.getResources(webDavUrl);
 		
 		for (DavResource res : resources) {
 		     System.out.println(res);
