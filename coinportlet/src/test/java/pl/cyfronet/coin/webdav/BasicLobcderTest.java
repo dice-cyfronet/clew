06d46984684a8a0d02ddfb6ddb43ca7be9a47b5b
Daniel Harezlak <d.harezlak@cyfronet.pl>
2012-11-26 12:28:43 +0100

    - LOBCDER tests enabled - configuration changed to include system properties

--- coinportlet/src/test/java/pl/cyfronet/coin/webdav/BasicLobcderTest.java
+++ coinportlet/src/test/java/pl/cyfronet/coin/webdav/BasicLobcderTest.java
@@ -1,6 +1,5 @@
 package pl.cyfronet.coin.webdav;
 
-import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -10,9 +9,9 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
-import org.apache.http.HttpException;
 import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.junit.Ignore;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -24,28 +23,58 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.googlecode.sardine.DavResource;
 import com.googlecode.sardine.Sardine;
 import com.googlecode.sardine.SardineFactory;
+import com.googlecode.sardine.util.SardineException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:/coinportlet-app-ctx.xml")
-public class LobcderTest {
-	private static final Logger log = LoggerFactory.getLogger(LobcderTest.class);
+public class BasicLobcderTest {
+	private static final Logger log = LoggerFactory.getLogger(BasicLobcderTest.class);
 	
 	@Value("${lobcder.user}") private String webDavUser;
 	@Value("${lobcder.password}") private String webDavPassword;
 	@Value("${lobcder.url}") private String webDavUrl;
 	
+	private Sardine sardine;
+	
+	@Before
+	public void initialize() throws KeyManagementException, SardineException,
+			NoSuchAlgorithmException {
+		sardine = SardineFactory.begin(webDavUser, webDavPassword, createNaiveSslSocketFactory());
+	}
+	
 	@Test
-	@Ignore
-	public void connectAndList() throws HttpException, IOException, KeyManagementException, NoSuchAlgorithmException {
+	public void connectAndList() throws SardineException {
 		log.info("Connecting to LOBCDER service at {}", webDavUrl);
-		Sardine sardine = SardineFactory.begin(webDavUser, webDavPassword, createNaiveSslSocketFactory());
 		List<DavResource> resources = sardine.getResources(webDavUrl);
 		
 		for (DavResource res : resources) {
 		     log.info(res.toString());
 		}
+		
+		Assert.assertNotNull(resources);
 	}
 	
+	@Test
+	public void createDirectory() throws SardineException {
+		String directoryName = createLobcderDirectoryName(String.valueOf(System.currentTimeMillis()));
+		sardine.createDirectory(directoryName);
+		Assert.assertTrue(sardine.exists(directoryName));
+		
+		//let's clean up
+		sardine.delete(directoryName);
+		Assert.assertTrue(!sardine.exists(directoryName));
+	}
+	
+	private String createLobcderDirectoryName(String name) {
+		String baseUrl = webDavUrl;
+		
+		if(!webDavUrl.endsWith("/")) {
+			baseUrl += "/";
+		}
+		
+		return baseUrl + name;
+	}
+
 	private SSLSocketFactory createNaiveSslSocketFactory() throws NoSuchAlgorithmException, KeyManagementException {
 		SSLContext sc = SSLContext.getInstance("SSL");
 		TrustManager[] trustAllCerts = new TrustManager[]{
