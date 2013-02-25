792f6b048eac2dfddcfdf508d54c7b85b9660eec
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-02-25 15:14:26 +0100

    client cleaned

--- coined/src/test/java/pl/cyfronet/coin/impl/air/client/WorkflowManagementTest.java
+++ coined/src/test/java/pl/cyfronet/coin/impl/air/client/WorkflowManagementTest.java
@@ -32,39 +32,8 @@ public class WorkflowManagementTest {
 		BeanFactory factory = (BeanFactory) appContext;
 		
 		WorkflowManagement wm = factory.getBean("wm-client", WorkflowManagement.class);
-//		JSONProvider provider = factory.getBean("jsonProvider", JSONProvider.class);
+
 		
-//		System.out.println(wm.getStatus("4f4b615c86648809b50004e2"));
-//		System.out.println(wm.getWorkflows());
-		
-		
-//		WorkflowStartRequest start = new WorkflowStartRequest();
-//		start.setDescription("my description");
-//		start.setName("workflowName");
-//		start.setPriority(55);
-//		start.setType(WorkflowType.portal);
-		
-//		System.out.println(start);		
-//		
-//		System.out.println(wm.startWorkflow(start));
-		
-//		wm.stopWorkflow("4f730f87866488401c000191");
-		
-//		wm.stopWorkflow("4f4dc97a866488548d000253");
-		
-//		System.out.println(wm.getWorkflows());
-//		wm.stopWorkflow("4f4dc75f866488548d00020f");
-//		System.out.println(wm.getWorkflows());
-		
-//		System.out.println("info: " + wm.getStatus("4f4dc984866488548d000268"));
-		
-//		System.out.println(wm.getInitialConfigurations("@neurist Services"));
-		
-		//wm.addAtomicServiceToWorkflow("4f4dc984866488548d000268", "4f4dde29866488548d0002fa");
-		//System.out.println(wm.getStatus("4f4e0bdb866488548d000f01"));
-		
-		System.out.println(wm.getWorkflows());
-		System.out.println(wm.getWorkflow("4f8eafde86648824da00b15b"));
 		
 	}
 }
