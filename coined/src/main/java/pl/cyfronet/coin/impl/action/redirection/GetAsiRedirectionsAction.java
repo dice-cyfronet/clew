0e0507ed9630a8c98f205341ef7845710f7c4c2d
tomek <tomek.bartynski@gmail.com>
2013-02-25 16:04:17 +0100

    implemented half of get reds action

--- coined/src/main/java/pl/cyfronet/coin/impl/action/redirection/GetAsiRedirectionsAction.java
+++ coined/src/main/java/pl/cyfronet/coin/impl/action/redirection/GetAsiRedirectionsAction.java
@@ -16,12 +16,21 @@
 
 package pl.cyfronet.coin.impl.action.redirection;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import pl.cyfronet.coin.api.RedirectionType;
 import pl.cyfronet.coin.api.beans.Redirection;
 import pl.cyfronet.coin.api.exception.CloudFacadeException;
+import pl.cyfronet.coin.impl.action.Action;
 import pl.cyfronet.coin.impl.action.AirAction;
+import pl.cyfronet.coin.impl.action.GetWorkflowDetailAction;
+import pl.cyfronet.coin.impl.air.client.ATPortMapping;
 import pl.cyfronet.coin.impl.air.client.AirClient;
+import pl.cyfronet.coin.impl.air.client.ApplianceType;
+import pl.cyfronet.coin.impl.air.client.PortMapping;
+import pl.cyfronet.coin.impl.air.client.Vms;
+import pl.cyfronet.coin.impl.air.client.WorkflowDetail;
 
 /**
  * @author <a href="mailto:mkasztelnik@gmail.com">Marek Kasztelnik</a>
@@ -30,20 +39,51 @@ public class GetAsiRedirectionsAction extends AirAction<List<Redirection>> {
 
 	private String asiId;
 	private String contextId;
+	private String username;
 
-	public GetAsiRedirectionsAction(String contextId, String asiId, AirClient air) {
+	public GetAsiRedirectionsAction(String contextId, String username, String asiId, AirClient air) {
 		super(air);
 		this.contextId = contextId;
+		this.username = username;
 		this.asiId = asiId;
 	}
 
 	@Override
 	public List<Redirection> execute() throws CloudFacadeException {
-		// TODO Auto-generated method stub
+		List<Redirection> redirections = new ArrayList<>();
+		Action<WorkflowDetail> getWfDetailAct = new GetWorkflowDetailAction(getAir(), contextId, username);
+		WorkflowDetail wfd = getWfDetailAct.execute();
+		if (wfd.getVms() == null || wfd.getVms().isEmpty()) { return redirections; }
+		Redirection red = null;
+		for (Vms asi : wfd.getVms()) {
+			if (asi.getVms_id().equals(asiId)) {
+				List<PortMapping> pms = asi.getInternal_port_mappings();
+				if (pms == null) { break; }
+				for(PortMapping pm : pms) {
+					red = new Redirection();
+					red.setFromPort(pm.getHeadnode_port());
+					red.setHost(pm.getHeadnode_ip());
+					red.setName(pm.getService_name());
+					red.setToPort(pm.getVm_port());
+					red.setType(RedirectionType.TCP);
+					redirections.add(red);
+				}
+				break;
+			}
+		}
+		/*
+		ApplianceType applType = getAir().getTypeFromVM(asiId);
+		if (applType == null) { return redirections; }
+		List<ATPortMapping> atpms = applType.getPort_mappings();
+		if (atpms == null || atpms.isEmpty()) { return redirections; }
+		for (ATPortMapping atpm : atpms) {
+			red = new Redirection();
+			red.setFromPort(fromPort);
+		}*/
 		// uzyc 2 metod AIR
 		// 1) pobrac workflow (patrz na klase workflow action) wf detail, wyciagnac vm, wyciagnac info o przekierowaniach tcp/ip
 		// 2) dla vm pobrac appliance type i z niego przekirowania http, patrz na getApplianceType z workflow action 
-		return null;
+		return redirections;
 	}
 
 	@Override
---
+++ coined/src/test/java/pl/cyfronet/coin/impl/action/redirection/GetAsiRedirectionsActionTest.java
@@ -0,0 +1,99 @@
+package pl.cyfronet.coin.impl.action.redirection;
+
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import pl.cyfronet.coin.api.beans.Redirection;
+import pl.cyfronet.coin.api.beans.Status;
+import pl.cyfronet.coin.api.beans.WorkflowType;
+import pl.cyfronet.coin.impl.action.Action;
+import pl.cyfronet.coin.impl.action.ActionTest;
+import pl.cyfronet.coin.impl.air.client.PortMapping;
+import pl.cyfronet.coin.impl.air.client.Vms;
+import pl.cyfronet.coin.impl.air.client.WorkflowDetail;
+
+public class GetAsiRedirectionsActionTest extends ActionTest {
+
+	private static final String ASI_ID = "siteId-vm-redirections";
+	private static final String CTX_ID = "redirectionsTest";
+	private static final String USERNAME = "redirectedUser";
+	private static final String WORKFLOW_NAME = "redirectedWorkflow";
+	
+	
+	private List<Redirection> redirections = null;
+	
+	
+	@Test
+	public void shouldGetAsiRedirections() {
+		givenAsiRedirectionsInAIR();
+		whenGetAsiRedirections();
+		thenAsiRedirectionsReturned();
+	}
+
+	private void givenAsiRedirectionsInAIR() {
+		WorkflowDetail wd = new WorkflowDetail();
+		wd.setId(CTX_ID);
+		wd.setVph_username(USERNAME);
+		wd.setName(WORKFLOW_NAME);
+		wd.setWorkflow_type(WorkflowType.development);
+		PortMapping sshMapping = new PortMapping();
+		sshMapping.setVm_port(22);
+		sshMapping.setHeadnode_port(222);
+		sshMapping.setHeadnode_ip("headnodeIp");
+		sshMapping.setService_name("ssh");
+		sshMapping.setHttp(false);
+		
+		PortMapping sshMapping2 = new PortMapping();
+		sshMapping2.setVm_port(22);
+		sshMapping2.setHeadnode_port(444);
+		sshMapping2.setHeadnode_ip("headnodeIp");
+		sshMapping2.setService_name("ssh");
+		sshMapping2.setHttp(false);
+
+		PortMapping vncMapping = new PortMapping();
+		vncMapping.setVm_port(5900);
+		vncMapping.setHeadnode_port(55900);
+		vncMapping.setHeadnode_ip("headnodeIp");
+		vncMapping.setService_name("vnc");
+		vncMapping.setHttp(false);
+
+		Vms vm1 = new Vms();
+		vm1.setAppliance_type("type1");
+		vm1.setAppliance_type_name("type1 name");
+		vm1.setName("vm1");
+		vm1.setState(Status.booting);
+		vm1.setVms_id(ASI_ID);
+		vm1.setInternal_port_mappings(Arrays.asList(sshMapping, vncMapping));
+		vm1.setUser_key("userKey1");
+
+		Vms vm2 = new Vms();
+		vm2.setAppliance_type("type2");
+		vm2.setAppliance_type_name("type2 name");
+		vm2.setName("vm2");
+		vm2.setState(Status.running);
+		vm2.setVms_id("id2");
+		vm2.setInternal_port_mappings(Arrays.asList(sshMapping2));
+		vm2.setUser_key("userKey2");
+
+		wd.setVms(Arrays.asList(vm1, vm2));
+
+		when(air.getWorkflow(CTX_ID)).thenReturn(wd);
+	}
+
+	private void whenGetAsiRedirections() {
+		Action<List<Redirection>> action = new GetAsiRedirectionsAction(CTX_ID, USERNAME, ASI_ID, air);
+		redirections = action.execute();
+	}
+
+	private void thenAsiRedirectionsReturned() {
+		assertNotNull(redirections);
+		assertEquals(redirections.size(), 2);
+	}
+
+}
