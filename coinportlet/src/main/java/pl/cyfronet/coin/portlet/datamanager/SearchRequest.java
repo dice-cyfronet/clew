24eadfb2485b696acddf835e003fb42759d84f37
Daniel Harezlak <d.harezlak@cyfronet.pl>
2013-03-26 17:35:58 +0100

    refs #1498 - name and creation date fields were added

--- coinportlet/src/main/java/pl/cyfronet/coin/portlet/datamanager/SearchRequest.java
+++ coinportlet/src/main/java/pl/cyfronet/coin/portlet/datamanager/SearchRequest.java
@@ -4,6 +4,8 @@ public class SearchRequest {
 	private String name;
 	private String startModificationDate;
 	private String stopModificationDate;
+	private String startCreationDate;
+	private String stopCreationDate;
 
 	public String getName() {
 		return name;
@@ -24,5 +26,19 @@ public class SearchRequest {
 	}
 	public void setStopModificationDate(String stopModificationDate) {
 		this.stopModificationDate = stopModificationDate;
+	}
+	
+	public String getStartCreationDate() {
+		return startCreationDate;
+	}
+	public void setStartCreationDate(String startCreationDate) {
+		this.startCreationDate = startCreationDate;
+	}
+	
+	public String getStopCreationDate() {
+		return stopCreationDate;
+	}
+	public void setStopCreationDate(String stopCreationDate) {
+		this.stopCreationDate = stopCreationDate;
 	}	
 }
