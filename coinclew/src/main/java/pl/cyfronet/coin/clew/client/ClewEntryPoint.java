2382223477c56e4caf311fcd83938e0fe9c3c5d5
Daniel Harezlak <d.harezlak@cyfronet.pl>
2013-08-21 15:24:21 +0200

    restygwt dependency added

--- coinclew/src/main/java/pl/cyfronet/coin/clew/client/ClewEntryPoint.java
+++ coinclew/src/main/java/pl/cyfronet/coin/clew/client/ClewEntryPoint.java
@@ -1,12 +1,35 @@
 package pl.cyfronet.coin.clew.client;
 
+import org.fusesource.restygwt.client.Method;
+import org.fusesource.restygwt.client.Resource;
+import org.fusesource.restygwt.client.TextCallback;
+
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class ClewEntryPoint implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
-		RootPanel.get().add(new Label("Hello!"));
+		RootPanel.get().add(new Button("Go!", new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				Resource resource = new Resource("http://0.0.0.0:3000/api/v1/security_policies");
+				resource.get().send(new TextCallback() {
+					@Override
+					public void onSuccess(Method method, String response) {
+						Window.alert(response);
+					}
+					
+					@Override
+					public void onFailure(Method method, Throwable exception) {
+						Window.alert(exception.getMessage());
+					}
+				});
+			}
+		}));
 	}
 }
