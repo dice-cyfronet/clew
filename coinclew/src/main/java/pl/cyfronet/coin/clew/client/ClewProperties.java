24f6aef793f108ccba9f764f7965b30a3c215ea8
Daniel Harezlak <d.harezlak@cyfronet.pl>
2013-08-26 14:07:13 +0200

    clew container logic implemented

---
+++ coinclew/src/main/java/pl/cyfronet/coin/clew/client/ClewProperties.java
@@ -0,0 +1,7 @@
+package pl.cyfronet.coin.clew.client;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface ClewProperties extends Constants {
+	String getContainerId();
+}
--- coinclew/src/main/java/pl/cyfronet/coin/clew/client/controller/ClewController.java
+++ coinclew/src/main/java/pl/cyfronet/coin/clew/client/controller/ClewController.java
@@ -3,6 +3,7 @@ package pl.cyfronet.coin.clew.client.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import pl.cyfronet.coin.clew.client.ClewProperties;
 import pl.cyfronet.coin.clew.client.widgets.dashboard.DashboardPresenter;
 
 import com.google.gwt.user.client.ui.RootPanel;
@@ -12,14 +13,16 @@ public class ClewController {
 	private static final Logger log = LoggerFactory.getLogger(ClewController.class);
 	
 	private DashboardPresenter dashboardPresenter;
+	private ClewProperties properties;
 	
 	@Inject
-	public ClewController(DashboardPresenter dashboardPresenter) {
+	public ClewController(DashboardPresenter dashboardPresenter, ClewProperties properties) {
 		this.dashboardPresenter = dashboardPresenter;
+		this.properties = properties;
 	}
 	
 	public void start() {
-		RootPanel.get().add(dashboardPresenter.getWidget());
+		RootPanel.get(properties.getContainerId()).add(dashboardPresenter.getWidget());
 		dashboardPresenter.load();
 		log.info("CLEW application successfully loaded");
 	}
